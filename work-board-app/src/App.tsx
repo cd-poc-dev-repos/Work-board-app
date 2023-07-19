9a1a1acf2c9e2d26ca0149d311c486a4442cbaf3
Christopher Dunn <christopher.dunnwk94@gmail.com>
2023-07-19 16:15:37 +0100

    added routing

--- work-board-app/src/App.tsx
+++ work-board-app/src/App.tsx
@@ -1,20 +1,27 @@
-import React from 'react';
-import SideBar from './common/components/SideBar';
-import SiteHeader from './common/components/SiteHeader';
-import * as Styled from './App.style';
-import WorkBoard from './pages/workboard';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SideBar from "./common/components/SideBar";
+import SiteHeader from "./common/components/SiteHeader";
+import WorkBoard from "./pages/workboard";
+import Dashboard from "./pages/dashboard";
+import * as Styled from "./App.style";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <SiteHeader />
+      <BrowserRouter>
+        <SiteHeader />
         <Styled.Container>
           <SideBar />
           <Styled.Content>
-            <WorkBoard />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/workboard" element={<WorkBoard />} />
+            </Routes>
           </Styled.Content>
         </Styled.Container>
+      </BrowserRouter>
     </div>
   );
 }
--- work-board-app/src/common/components/SideBar/SideBar.tsx
+++ work-board-app/src/common/components/SideBar/SideBar.tsx
@@ -1,8 +1,22 @@
 import React from "react";
-import * as Styled from './SideBar.style';
+import { Link } from "react-router-dom";
+import * as Styled from "./SideBar.style";
 
 const SideBar = () => {
-    return <Styled.Container />;
-}
+  return (
+    <Styled.Container>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/workboard">WorkBoard</Link>
+          </li>
+        </ul>
+      </nav>
+    </Styled.Container>
+  );
+};
 
 export default SideBar;
